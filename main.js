8af9bc735114ddb9a8c6c214bfb29ddf5bcf0a89
Ous1213 <odiallo1@binghamton.edu>
2024-03-04 20:11:16 -0500

    nearly finish

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
 const myBody = document.querySelector("body");
+let amount;
+let userChoicebtn = document.getElementById("userChoice");
+userChoicebtn.addEventListener("click", () => {
+  amount = prompt("Enter a amount");
+  if (isNaN(amount) || amount > 100) {
+    alert("Enter a valid number <= 100");
+  } else {
+    resetGrid();
+    setGrid();
+  }
+});
+
+
+function resetGrid(){
+  const container = document.getElementById('gridContainer');
+  if (container){
+    myBody.removeChild(container);
+  }
+}
+function randColorNum() {
+  return Math.floor(Math.random() * 256);
+}
+
+function randomRGB() {
+  return `rgb(${randColorNum()},${randColorNum()},${randColorNum()})`;
+}
 
 function setGrid() {
   const container = document.createElement("div");
-  container.style.flexWrap = "wrap";
+  container.id = "gridContainer";
+  container.style.height = "800px";
+  container.style.width = "800px";
   container.style.display = "flex";
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+  container.style.flexWrap = "wrap";
+  for (let i = 0; i < Number(amount); i++) {
+    const rowContainer = document.createElement("div");
+    rowContainer.style.display = "flex";
+    for (let j = 0; j < Number(amount); j++) {
       const innerDiv = document.createElement("div");
-      // innerDiv.textContent = " -";
-      const width = "50px";
-      const height = "50px";
-      innerDiv.style.height = height;
-      innerDiv.style.width = width;
-      innerDiv.style.border = "10px solid pink";
+      const width = 800 / Number(amount);
+      innerDiv.style.width = width + "px";
+      innerDiv.style.border = "1px solid grey";
       innerDiv.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "red";
-        // create with intial height and width initally not with event listnere
+        const color = randomRGB();
+        e.target.style.backgroundColor = color;
       });
 
-      container.appendChild(innerDiv);
-      myBody.appendChild(container);
+      rowContainer.appendChild(innerDiv);
     }
+    container.appendChild(rowContainer);
   }
+  myBody.appendChild(container);
 }
-setGrid();
